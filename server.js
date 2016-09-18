6f88f3001f4b4d32e28d0389665a727405bdeab2
abhinandan-raju <abhinandan.raju@gmail.com>
2016-09-18 21:19:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,131 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var common = {
+	aboutme: {
+		title: 'About Me | Abhinandan Raju',
+		heading: 'About Me page',
+		date: 'Sep 5, 2016',
+		content: `
+				<p> 
+					This page talks about me. 
+				</p>
+				<p> 
+					I have developed this page by myself. 
+				</p>
+				<p> 
+					Lets see if this works. 
+				</p>
+				<p> 
+					This is my common content page for all three pages.
+				</p>`
+		
+	},
+	aboutmylistings: { 
+		title: 'Lisitings | Abhinandan Raju',
+		heading: 'Lisitings page',
+		date: 'Sep 10, 2016',
+		content: `
+				<p> 
+					My listings include many.
+				</p>
+				<p> 
+					I have developed this page by myself. 
+				</p>
+				<p> 
+					Lets see if this works. 
+				</p>
+				<p> 
+					This is my common content page for all three pages.
+				</p>`},
+	},
+	aboutmyjob: {
+		title: 'My Job | Abhinandan Raju',
+		heading: 'My job page',
+		date: 'Sep 15, 2016',
+		content: `
+				<p> 
+					I work as an Associate at Cognizant. 
+				</p>
+				<p> 
+					I have developed this page by myself. 
+				</p>
+				<p> 
+					Lets see if this works. 
+				</p>
+				<p> 
+					This is my common content page for all three pages.
+				</p>
+				`
+				};
+				
+				
+
+
+var content = {
+	title: 'About Me | Abhinandan Raju',
+	heading: 'About Me page',
+	date: 'Sep 5, 2016',
+	content: `
+			<p> 
+				This page talks about me. 
+			</p>
+			<p> 
+				I have developed this page by myself. 
+			</p>
+			<p> 
+				Lets see if this works. 
+			</p>
+			<p> 
+				This is my common content page for all three pages.
+			</p>`
+		
+};
+
+function createTemplate (data) {
+	var title = data.title;
+	var date = data.date;
+	var heading = data.heading;
+	var content = data.content;
+
+	var html Template = `
+	<html>
+		<head>
+			<title>
+				${title}
+			</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<link href="/ui/style.css" rel="stylesheet" />
+		</head>
+		<body> 
+			<div class="container">
+					<div>
+						<a href="/">Home</a>
+					</div>
+					<hr/>
+					<h3> 
+						${heading}
+					</h3>
+					<div>
+						${date}
+					</div>
+					<div>
+						${content}
+					</div>
+			</div>
+		</body>
+	</html>
+	`;
+	return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:aboutName', function (req, res) {
+	var aboutName = req.params.aboutName;
+	res.send(createTemplate(common[aboutName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -22,3 +145,4 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
