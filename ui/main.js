bc2c6453d41012603ba52e61f3c86962da7752c7
abhinandan-raju <abhinandan.raju@gmail.com>
2016-09-25 22:02:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,6 +21,6 @@ button.onclick = function () {
    };
    
    //Make the request
-   request.open('GET', 'http://abhinandan-raju.imad.hasura-app.io/', true);
+   request.open('GET', 'http://abhinandan-raju.imad.hasura-app.io/counter', true);
    request.send(null);
 };
