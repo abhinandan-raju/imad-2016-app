7b75c5399624cbf19e19283b542e0974dbd61766
abhinandan-raju <abhinandan.raju@gmail.com>
2016-09-25 21:58:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,14 +7,12 @@ button.onclick = function () {
     //Create a request object
     var request = new XMLHttpRequest();
     
-    
     //Capture the response and store it in a variable
     request.onreadystatechange = function (){
-        if (request.readyState === XMLHttpRequest.DONE){  
+        if (request.readyState === XMLHttpRequest.DONE) {  
             //Take some action
             if (request.status === 200) {
                 var counter = request.responseText;
-                
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
@@ -25,5 +23,4 @@ button.onclick = function () {
    //Make the request
    request.open('GET', 'http://abhinandan-raju.imad.hasura-app.io/', true);
    request.send(null);
-    
 };
