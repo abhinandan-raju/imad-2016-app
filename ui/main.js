86fe29bf651b03179cb22018942a4bd3d902c25c
abhinandan-raju <abhinandan.raju@gmail.com>
2016-09-25 21:43:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,29 @@
 //Counter code
 
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function () {
     
-    //Make a request to the counter endpoint
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
     
     //Capture the response and store it in a variable
+    request.onreadystatechange = function (){
+        if (request.readyState === XMLHttpRequest.DONE){  
+            //Take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+    } 
+    //Not done yet
+   };
+   
+   //Make the request
+   request.open('GET', 'http://abhinandan-raju.imad.hasura-app.io/', true);
+   request.send(null);
     
-    //Render the variable in the correct span
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
 };
